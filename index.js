8729c974067f88988a71a40be40a0b4c881c0cc7
Jeff Dickey <dickeyxxx@gmail.com>
2016-05-18 13:40:46 -0700

    finished v1

--- index.js
+++ index.js
@@ -2,12 +2,30 @@
 
 const co = require('co')
 const cli = require('heroku-cli-util')
+const {Dyno} = require('heroku-run')
 
 function * run (context, heroku) {
+  let opts = {
+    heroku: heroku,
+    app: context.app,
+    command: `mkdir vim
+    curl https://s3.amazonaws.com/heroku-vim/vim-7.3.tar.gz --location --silent | tar xz -C vim
+      export PATH=$PATH:/app/vim/bin
+    bash`,
+    env: context.flags.env,
+    showStatus: false,
+    attach: true
+  }
+  if (!opts.command) throw new Error('Usage: heroku run COMMAND\n\nExample: heroku run bash')
+
+  let dyno = new Dyno(opts)
+  yield dyno.start()
 }
 
 const vim = {
   topic: 'vim',
+  needsAuth: true,
+  needsApp: true,
   run: cli.command(co.wrap(run))
 }
 
