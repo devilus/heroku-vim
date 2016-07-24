7663c119a4c6d558df8b348a2da81bdbbd6d5cd9
Yu Wang <wangyuhere@gmail.com>
2016-07-24 23:42:17 +0200

    Add $VIMRUNTIME
    By default, the VIMRUNTIME is `/app/vimbin/share/vim` on Heroku
    This fixes #1

--- index.js
+++ index.js
@@ -11,6 +11,7 @@ function * run (context, heroku) {
     command: `mkdir vim
     curl https://s3.amazonaws.com/heroku-vim/vim-7.3.tar.gz --location --silent | tar xz -C vim
       export PATH=$PATH:/app/vim/bin
+      export VIMRUNTIME=/app/vim/share/vim/vim73
     bash`,
     env: context.flags.env,
     showStatus: false,
